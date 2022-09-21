9bca143179833ed3d59285f85be75a60fcd12af7
FranAlcoba66 <falcoba@picallex.com>
2022-09-21 13:12:35 -0300

    validaciones

--- POO/Usuario2.py
+++ POO/Usuario2.py
@@ -1,4 +1,9 @@
+from curses.ascii import isdigit
+import re
 from xml.etree.ElementTree import tostring
+
+
+
 
 class Usuario :
     def __init__(self,nombre,apellido,email,contraseña):
@@ -41,7 +46,53 @@ class Usuario :
 
     def __str__ (self):
         return 'Nombre: '+ str(self.__nombre)+ ', Apellido : '+ str(self.__apellido)+', Email: '+ str(self.__email) +', Contraseña: '+ str(self.__contraseña)
+def verificarNombre():
+        largo=False
+        while largo==False :
+            usuario1.nombre =str(input('Ingrese nombre: '))
+            if len(usuario1.nombre) >=6 and len(usuario1.nombre)<=12:
+                largo=True
+            else:
+                print('Su nombre debe tener entre 6 y 12 caracteres ')
+def verificarApellido():
+        largo=False
+        while largo==False :
+            usuario1.apellido =str(input('Ingrese apellido: '))
+            if len(usuario1.apellido) >=6 and len(usuario1.apellido)<=12:
+                largo=True
+            else:
+                print('Su apellido debe tener entre 6 y 12 caracteres ')
 
+def verificarContraseña():
+
+        largo=False
+        numeros=False
+        mayus=False
+        minus=False
+        while largo==False or minus==False or numeros==False or mayus==False:
+            usuario1.contraseña =str(input('Ingrese contraseña: '))
+            if len(usuario1.contraseña) >= 6:
+                largo=True
+                if any(chr.isdigit() for chr in usuario1.contraseña):
+                    numeros=True
+                    if any(chr.islower() for chr in usuario1.contraseña ):
+                        minus=True
+                        if any(chr.isupper() for chr in usuario1.contraseña):
+                            print('Contraseña correcta')
+                            mayus=True
+                        else:
+                            print('Su contraseña debe tener al menos una mayuscula')
+                    else:
+                        print('Su contraseña debe tener al menos una minuscula')
+                else:
+                    print('Su contraseña debe tener al menos un numero')
+            else:
+                print('Su contraseña debe tener al menos 6 caracteres')
+
+def validate_email(arg_email):
+        EMAIL_REGEX = re.compile(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$")
+        is_valid = False if not EMAIL_REGEX.match(arg_email) else True
+        print("Direccion de correo valida" if is_valid else "Direccion de correo invalida.")
 
 usuario1 = Usuario('','','','')
 
@@ -60,18 +111,19 @@ if menu =='roja':
 
 elif menu=='azul':
     print('continua con su vida normal')
-print(usuario1)
 
-usuario1.nombre=input('Ingrese su nombre: ')
-print(usuario1.nombre)
 
-usuario1.apellido=input('Ingrese su apellido: ')
-print(usuario1.apellido)
+verificarNombre()
+
+verificarApellido()
 
 usuario1.email=input('Ingrese su email: ')
-print(usuario1.email)
+arg_email=usuario1.email
+validate_email(arg_email)
 
-usuario1.contraseña=input('Ingrese su contraseña: ')
-print(usuario1.contraseña)
 
-print(usuario1)
+verificarContraseña()
+
+
+print('Usuario generado correctamente :',usuario1)
+
